662e6df58c690ba8db86a6414473635eb8debcfd
Tsuguko35 <carpio.johnjazpher.dc.3188@gmail.com>
2024-05-30 09:30:14 +0800

    new images

--- client/src/components/reusable/ProductGallery.js
+++ client/src/components/reusable/ProductGallery.js
@@ -25,25 +25,25 @@ function ProductGallery() {
         <div className="products">
           <div className="product">
             <img
-              src="https://img.freepik.com/free-photo/living-room-modern-apartment-with-two-identical-sofas-opposite-each-other_181624-14768.jpg?t=st=1717027863~exp=1717031463~hmac=0ead8d100a87c2901be396daec32e9401fc2cf2ac951aa30d12a8a6a363b0f8b&w=1380"
+              src="https://images.pexels.com/photos/2635038/pexels-photo-2635038.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
               alt="product"
             />
           </div>
           <div className="product">
             <img
-              src="https://img.freepik.com/free-photo/3d-rendering-loft-luxury-living-room-with-shelf-near-dining-table-counter_105762-2105.jpg?t=st=1717027916~exp=1717031516~hmac=f7e4f7e5fe3f446c9a863ac5fcd5e0011109c9269a1cb12c0640d9ab64d13b3c&w=1380"
+              src="https://images.pexels.com/photos/2988860/pexels-photo-2988860.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
               alt="product"
             />
           </div>
           <div className="product">
             <img
-              src="https://img.freepik.com/free-photo/3d-rendering-loft-scandinavian-living-room-with-working-table-bookshelf_105762-2162.jpg?t=st=1717027943~exp=1717031543~hmac=874101829ce8121ed018a2d6b3c536ce50c4045fb80c2083c7f65a38dbc2e291&w=1380"
+              src="https://images.pexels.com/photos/1571468/pexels-photo-1571468.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
               alt="product"
             />
           </div>
           <div className="product">
             <img
-              src="https://img.freepik.com/free-photo/beautiful-shot-modern-house-kitchen_181624-4007.jpg?t=st=1717027963~exp=1717031563~hmac=39941f7d6192e68fb85b8f5563646af2f19bd8077af5584044852a4ea92a0896&w=1380"
+              src="https://images.pexels.com/photos/1643383/pexels-photo-1643383.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
               alt="product"
             />
           </div>
@@ -51,49 +51,49 @@ function ProductGallery() {
             <React.Fragment>
               <div className="product">
                 <img
-                  src="https://img.freepik.com/free-photo/living-room-modern-apartment-with-two-identical-sofas-opposite-each-other_181624-14768.jpg?t=st=1717027863~exp=1717031463~hmac=0ead8d100a87c2901be396daec32e9401fc2cf2ac951aa30d12a8a6a363b0f8b&w=1380"
+                  src="https://images.pexels.com/photos/2635038/pexels-photo-2635038.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
                   alt="product"
                 />
               </div>
               <div className="product">
                 <img
-                  src="https://img.freepik.com/free-photo/3d-rendering-loft-luxury-living-room-with-shelf-near-dining-table-counter_105762-2105.jpg?t=st=1717027916~exp=1717031516~hmac=f7e4f7e5fe3f446c9a863ac5fcd5e0011109c9269a1cb12c0640d9ab64d13b3c&w=1380"
+                  src="https://images.pexels.com/photos/2988860/pexels-photo-2988860.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
                   alt="product"
                 />
               </div>
               <div className="product">
                 <img
-                  src="https://img.freepik.com/free-photo/3d-rendering-loft-scandinavian-living-room-with-working-table-bookshelf_105762-2162.jpg?t=st=1717027943~exp=1717031543~hmac=874101829ce8121ed018a2d6b3c536ce50c4045fb80c2083c7f65a38dbc2e291&w=1380"
+                  src="https://images.pexels.com/photos/1571468/pexels-photo-1571468.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
                   alt="product"
                 />
               </div>
               <div className="product">
                 <img
-                  src="https://img.freepik.com/free-photo/beautiful-shot-modern-house-kitchen_181624-4007.jpg?t=st=1717027963~exp=1717031563~hmac=39941f7d6192e68fb85b8f5563646af2f19bd8077af5584044852a4ea92a0896&w=1380"
+                  src="https://images.pexels.com/photos/1643383/pexels-photo-1643383.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
                   alt="product"
                 />
               </div>
               <div className="product">
                 <img
-                  src="https://img.freepik.com/free-photo/living-room-modern-apartment-with-two-identical-sofas-opposite-each-other_181624-14768.jpg?t=st=1717027863~exp=1717031463~hmac=0ead8d100a87c2901be396daec32e9401fc2cf2ac951aa30d12a8a6a363b0f8b&w=1380"
+                  src="https://images.pexels.com/photos/2635038/pexels-photo-2635038.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
                   alt="product"
                 />
               </div>
               <div className="product">
                 <img
-                  src="https://img.freepik.com/free-photo/3d-rendering-loft-luxury-living-room-with-shelf-near-dining-table-counter_105762-2105.jpg?t=st=1717027916~exp=1717031516~hmac=f7e4f7e5fe3f446c9a863ac5fcd5e0011109c9269a1cb12c0640d9ab64d13b3c&w=1380"
+                  src="https://images.pexels.com/photos/2988860/pexels-photo-2988860.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
                   alt="product"
                 />
               </div>
               <div className="product">
                 <img
-                  src="https://img.freepik.com/free-photo/3d-rendering-loft-scandinavian-living-room-with-working-table-bookshelf_105762-2162.jpg?t=st=1717027943~exp=1717031543~hmac=874101829ce8121ed018a2d6b3c536ce50c4045fb80c2083c7f65a38dbc2e291&w=1380"
+                  src="https://images.pexels.com/photos/1571468/pexels-photo-1571468.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
                   alt="product"
                 />
               </div>
               <div className="product">
                 <img
-                  src="https://img.freepik.com/free-photo/beautiful-shot-modern-house-kitchen_181624-4007.jpg?t=st=1717027963~exp=1717031563~hmac=39941f7d6192e68fb85b8f5563646af2f19bd8077af5584044852a4ea92a0896&w=1380"
+                  src="https://images.pexels.com/photos/1643383/pexels-photo-1643383.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
                   alt="product"
                 />
               </div>
