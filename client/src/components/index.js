99c69ee782d39887763b40c30c32a286ce5a04d3
Tsuguko35 <carpio.johnjazpher.dc.3188@gmail.com>
2024-07-09 13:06:20 +0800

    deleted

--- client/src/components/index.js
+++
@@ -1,4 +0,0 @@
-import LandingPageHeader from "./LandingPage/LandingPageHeader";
-import NavBar from "./NavBar";
-
-export { LandingPageHeader, NavBar };
--- client/src/utils/index.js
+++
@@ -1,3 +0,0 @@
-import { GetWindowWidth } from "./getWindowWidth";
-
-export { GetWindowWidth };
